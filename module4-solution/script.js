f460576e3fcde7345cf4f7732e5334363904f9d4
Vaidehi13 <64651061+Vaidehi13@users.noreply.github.com>
2020-05-17 23:34:11 +0530

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,1 +1,17 @@
 
+var names = ["John", "Vivek", "Janhvi", "Simran",  "Ajay"];
+var letter,compare,i,lower;
+for(i=0;i<names.length;i++)
+{
+    lower = names[i].toLowerCase();	
+	letter = lower.charAt(0);
+    compare = letter.localeCompare("j");
+    if(compare == 0)
+    {
+	  goodbyeSpeaker.sayGoodBye(names[i]);
+    }
+    else
+    {
+	  helloSpeaker.sayHello(names[i]);
+    }
+}
